1935d970a4349297ce3e82607d1f2c6e655869dd
vishal-s-k <50524633+vishal-s-k@users.noreply.github.com>
2019-06-10 09:23:05 +0530

    Update settings.py

--- simple_social_clone/simplesocial/simplesocial/settings.py
+++ simple_social_clone/simplesocial/simplesocial/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = "test"
 LOGOUT_REDIRECT_URL = "thanks"
