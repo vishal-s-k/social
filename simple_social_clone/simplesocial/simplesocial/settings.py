1093e496c781b514a6357504f409cb75b16067c5
vishal-s-k <50524633+vishal-s-k@users.noreply.github.com>
2019-06-10 11:03:32 +0530

    Update settings.py

--- simple_social_clone/simplesocial/simplesocial/settings.py
+++ simple_social_clone/simplesocial/simplesocial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://immense-citadel-66280.herokuapp.com/']
+ALLOWED_HOSTS = ['v-coterie.herokuapp.com']
 
 
 # Application definition
